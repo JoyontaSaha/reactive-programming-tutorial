c2426f5ff39599dfe0b01f9aa6e67420fea12867
JoyontaSaha <joyontasaha95@gmail.com>
2022-03-02 23:21:36 +0600

    map filter filtermap flatmap operators implemented with junit test

--- src/main/java/services/FluxAndMonoServices.java
+++ src/main/java/services/FluxAndMonoServices.java
@@ -3,20 +3,27 @@ package services;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.Random;
 
 public class FluxAndMonoServices {
     public static void main(String[] args) {
         FluxAndMonoServices fluxAndMonoServices = new FluxAndMonoServices();
 
         fluxAndMonoServices.fruitsFlux()
-                .subscribe(fruit -> {
-                    System.out.println("Flux Fruit :: "+ fruit);
+                .subscribe(s -> {
+                    System.out.println("Flux Fruit :: "+ s);
                 });
 
         fluxAndMonoServices.fruitMono()
-                .subscribe(fruit -> {
-                    System.out.println("Mono Fruit :: "+ fruit);
+                .subscribe(s -> {
+                    System.out.println("Mono Fruit :: "+ s);
+                });
+
+        fluxAndMonoServices.fruitMonoMap()
+                .subscribe(s -> {
+                    System.out.println("Map Mono Fruit :: "+ s);
                 });
 
     }
@@ -27,5 +34,51 @@ public class FluxAndMonoServices {
 
     public Mono<String> fruitMono() {
         return Mono.just("Apple").log();
+    }
+
+    public Flux<String> fruitsFluxMap() {
+        return Flux.fromIterable(List.of("Mango", "Orange", "Banana"))
+                .map(String :: toUpperCase)
+                .log();
+    }
+
+    public Mono<String> fruitMonoMap() {
+        return Mono.just("Apple")
+                .map(String :: toUpperCase)
+                .log();
+    }
+
+    public Flux<String> fruitsFluxFilter(int limit) {
+        return Flux.fromIterable(List.of("Mango", "Orange", "Banana"))
+                .filter(s -> s.length() > limit)
+                .log();
+    }
+
+    public Flux<String> fruitsFluxFilterMap(int limit) {
+        return Flux.fromIterable(List.of("Mango", "Orange", "Banana"))
+                .filter(s -> s.length() > limit)
+                .map(String :: toUpperCase)
+                .log();
+    }
+
+    public Flux<String> fruitsFluxFlatMap() {
+        return Flux.fromIterable(List.of("Mango", "Orange", "Banana"))
+                .flatMap(s -> Flux.just(s.split("")))
+                .log();
+    }
+
+    public Flux<String> fruitsFluxFlatMapAsync() {
+        return Flux.fromIterable(List.of("Mango", "Orange", "Banana"))
+                .flatMap(s -> Flux.just(s.split(""))
+                .delayElements(Duration.ofMillis(
+                        new Random().nextInt(1000)
+                )))
+                .log();
+    }
+
+    public Mono<List<String>> fruitMonoFlatMap() {
+        return Mono.just("Apple")
+                .flatMap(s -> Mono.just(List.of(s.split(""))))
+                .log();
     }
 }
--- src/test/java/services/FluxAndMonoServicesTest.java
+++ src/test/java/services/FluxAndMonoServicesTest.java
@@ -17,7 +17,7 @@ class FluxAndMonoServicesTest {
 
         StepVerifier.create(fruitsFlux)
                 .expectNext("Mango", "Orange", "Banana")
-                .expectComplete();
+                .verifyComplete();
     }
 
     @Test
@@ -28,6 +28,83 @@ class FluxAndMonoServicesTest {
 
          StepVerifier.create(fruitMono)
                  .expectNext("Apple")
-                 .expectComplete();
+                 .verifyComplete();
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("FruitsFluxMap() Method Testing")
+    void testFruitsFluxMap() {
+        var fruitsFluxMap = fluxAndMonoServices.fruitsFluxMap();
+
+        StepVerifier.create(fruitsFluxMap)
+                .expectNext("MANGO", "ORANGE", "BANANA")
+                .verifyComplete();
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("FruitMonoMap() Method Testing")
+    void testFruitMonoMap() {
+        var fruitMonoMap = fluxAndMonoServices.fruitMonoMap();
+
+        StepVerifier.create(fruitMonoMap)
+                .expectNext("APPLE")
+                .verifyComplete();
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("FruitsFluxFilter() Method Testing")
+    void testFruitsFluxFilter() {
+        var fruitsFluxFilter = fluxAndMonoServices.fruitsFluxFilter(5);
+
+        StepVerifier.create(fruitsFluxFilter)
+                .expectNextCount(2)
+                .verifyComplete();
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("FruitsFluxFilterMap() Method Testing")
+    void testFruitsFluxFilterMap() {
+        var fruitsFluxFilterMap = fluxAndMonoServices.fruitsFluxFilterMap(5);
+
+        StepVerifier.create(fruitsFluxFilterMap)
+                .expectNext("ORANGE", "BANANA")
+                .verifyComplete();
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("FruitsFluxFlatMap() Method Testing")
+    void testFruitsFluxFlatMap() {
+        var fruitsFluxFlatMap = fluxAndMonoServices.fruitsFluxFlatMap();
+
+        StepVerifier.create(fruitsFluxFlatMap)
+                .expectNextCount(("Mango" + "Orange" + "Banana").length())
+                .verifyComplete();
+    }
+
+    @Test
+    @Order(8)
+    @DisplayName("FruitsFluxFlatMapAsync() Method Testing")
+    void testFruitsFluxFlatMapAsync() {
+        var fruitsFluxFlatMapAsync = fluxAndMonoServices.fruitsFluxFlatMapAsync();
+
+        StepVerifier.create(fruitsFluxFlatMapAsync)
+                .expectNextCount(("Mango" + "Orange" + "Banana").length())
+                .verifyComplete();
+    }
+
+    @Test
+    @Order(9)
+    @DisplayName("FruitMonoFlatMap() Method Testing")
+    void testFruitMonoFlatMap() {
+        var fruitMonoFlatMap = fluxAndMonoServices.fruitMonoFlatMap();
+
+        StepVerifier.create(fruitMonoFlatMap)
+                .expectNextCount(1)
+                .verifyComplete();
     }
 }
