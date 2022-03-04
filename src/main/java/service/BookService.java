935a304c49ee3fccfd3bfd2c5c2333a40a1f91c4
JoyontaSaha <joyontasaha95@gmail.com>
2022-03-04 13:16:15 +0600

    retry retry n retry when implemented with mockitto test

--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -4,9 +4,13 @@ import domain.Book;
 import domain.Review;
 import exception.BookException;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+import reactor.util.retry.RetryBackoffSpec;
 
+import java.time.Duration;
 import java.util.List;
 
 @Slf4j
@@ -35,6 +39,70 @@ public class BookService {
                     return new BookException("Exception occurred while fetching Books");
                 })
                 .log();
+    }
+
+    public Flux<Book> getAllBooksRetry() {
+
+        var allBooks = bookInfoService.getAllBooksInfo();
+        return allBooks
+                .flatMap(bookInfo -> {
+                    Mono<List<Review>> reviews =
+                            reviewService.getReviewsByBookId(bookInfo.getBookId()).collectList();
+                    return reviews
+                            .map(review -> new Book(bookInfo, review));
+                })
+                .onErrorMap(throwable -> {
+                    log.error("Exception is {}", throwable);
+                    return new BookException("Exception occurred while fetching Books");
+                })
+                .retry()
+                .log();
+    }
+
+    public Flux<Book> getAllBooksRetryN(int n) {
+
+        var allBooks = bookInfoService.getAllBooksInfo();
+        return allBooks
+                .flatMap(bookInfo -> {
+                    Mono<List<Review>> reviews =
+                            reviewService.getReviewsByBookId(bookInfo.getBookId()).collectList();
+                    return reviews
+                            .map(review -> new Book(bookInfo, review));
+                })
+                .onErrorMap(throwable -> {
+                    log.error("Exception is {}", throwable);
+                    return new BookException("Exception occurred while fetching Books");
+                })
+                .retry(n)
+                .log();
+    }
+
+    public Flux<Book> getAllBooksRetryWhen() {
+
+        var allBooks = bookInfoService.getAllBooksInfo();
+        return allBooks
+                .flatMap(bookInfo -> {
+                    Mono<List<Review>> reviews =
+                            reviewService.getReviewsByBookId(bookInfo.getBookId()).collectList();
+                    return reviews
+                            .map(review -> new Book(bookInfo, review));
+                })
+                .onErrorMap(throwable -> {
+                    log.error("Exception is {}", throwable);
+                    return new BookException("Exception occurred while fetching Books");
+                })
+                .retryWhen(getRetrySpecs())
+                .log();
+    }
+
+    private RetryBackoffSpec getRetrySpecs() {
+        return Retry.backoff(
+                        3,
+                        Duration.ofMillis(1000)
+                ).filter(throwable -> throwable instanceof BookException)
+                .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) ->
+                        Exceptions.propagate(retrySignal.failure())
+                );
     }
 
     public Mono<Book> getBookById(Long bookId) {
--- src/test/java/service/BookServiceMockTest.java
+++ src/test/java/service/BookServiceMockTest.java
@@ -54,4 +54,51 @@ import static org.mockito.Mockito.when;
                 .expectError(BookException.class)
                 .verify();
     }
+
+    @Test
+    void testGetAllBooksOnErrorRetry() {
+        Mockito.when(bookInfoService.getAllBooksInfo())
+                .thenCallRealMethod();
+
+        Mockito.when(reviewService.getReviewsByBookId(Mockito.anyLong()))
+                .thenThrow(new IllegalArgumentException("Exception using test"));
+
+        var allBooks = bookService.getAllBooksRetry();
+
+        StepVerifier.create(allBooks)
+                .expectError(BookException.class)
+                .verify();
+    }
+
+    @Test
+    void testGetAllBooksOnErrorRetryN() {
+        Mockito.when(bookInfoService.getAllBooksInfo())
+                .thenCallRealMethod();
+
+        Mockito.when(reviewService.getReviewsByBookId(Mockito.anyLong()))
+                .thenThrow(new IllegalArgumentException("Exception using test"));
+
+        var allBooks = bookService.getAllBooksRetryN(3);
+
+        StepVerifier.create(allBooks)
+                .expectError(BookException.class)
+                .verify();
+    }
+
+    @Test
+    void testGetAllBooksOnErrorRetryWhen() {
+        Mockito.when(bookInfoService.getAllBooksInfo())
+                .thenCallRealMethod();
+
+        Mockito.when(reviewService.getReviewsByBookId(Mockito.anyLong()))
+                .thenThrow(new IllegalArgumentException("Exception using test"));
+
+        var allBooks = bookService.getAllBooksRetryWhen();
+
+        StepVerifier.create(allBooks)
+                .expectError(BookException.class)
+                .verify();
+    }
+
+
 }
